3063652ab96710295f7feab30e45dad77901ce36
P <P@DESKTOP-GRLCLV2>
2023-05-09 12:43:41 +1000

    Add project files.

---
+++ RichPresenceHub/Form1.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace RichPresenceHub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LargeImg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LargeImgTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SmallImg = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SmallImgTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TimeTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.details = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Btn1Txt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Btn1URL = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Btn2Txt = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Btn2URL = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.PartyIDTxt = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.MinPlayers = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.MaxPlayers = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(13, 13);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.details);
+            this.panel1.Controls.Add(this.MaxPlayers);
+            this.panel1.Controls.Add(this.MinPlayers);
+            this.panel1.Controls.Add(this.PartyIDTxt);
+            this.panel1.Controls.Add(this.Btn2URL);
+            this.panel1.Controls.Add(this.Btn2Txt);
+            this.panel1.Controls.Add(this.Btn1URL);
+            this.panel1.Controls.Add(this.Btn1Txt);
+            this.panel1.Controls.Add(this.TimeTxt);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.SmallImg);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.LargeImg);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.SmallImgTxt);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.LargeImgTxt);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.state);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.IDTxt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 422);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // IDTxt
+            // 
+            this.IDTxt.Location = new System.Drawing.Point(100, 4);
+            this.IDTxt.Name = "IDTxt";
+            this.IDTxt.Size = new System.Drawing.Size(165, 20);
+            this.IDTxt.TabIndex = 1;
+            this.IDTxt.Text = "1082522137677529118";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "State:";
+            // 
+            // state
+            // 
+            this.state.Location = new System.Drawing.Point(100, 54);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(165, 20);
+            this.state.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Large Image:";
+            // 
+            // LargeImg
+            // 
+            this.LargeImg.Location = new System.Drawing.Point(100, 80);
+            this.LargeImg.Name = "LargeImg";
+            this.LargeImg.Size = new System.Drawing.Size(165, 20);
+            this.LargeImg.TabIndex = 1;
+            this.LargeImg.Text = "allseeingphone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Large Image Text:";
+            // 
+            // LargeImgTxt
+            // 
+            this.LargeImgTxt.Location = new System.Drawing.Point(100, 106);
+            this.LargeImgTxt.Name = "LargeImgTxt";
+            this.LargeImgTxt.Size = new System.Drawing.Size(165, 20);
+            this.LargeImgTxt.TabIndex = 1;
+            this.LargeImgTxt.Text = "Testing";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Small Image:";
+            // 
+            // SmallImg
+            // 
+            this.SmallImg.Location = new System.Drawing.Point(100, 132);
+            this.SmallImg.Name = "SmallImg";
+            this.SmallImg.Size = new System.Drawing.Size(165, 20);
+            this.SmallImg.TabIndex = 1;
+            this.SmallImg.Text = "darkwork";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Small Image Text:";
+            // 
+            // SmallImgTxt
+            // 
+            this.SmallImgTxt.Location = new System.Drawing.Point(100, 158);
+            this.SmallImgTxt.Name = "SmallImgTxt";
+            this.SmallImgTxt.Size = new System.Drawing.Size(165, 20);
+            this.SmallImgTxt.TabIndex = 1;
+            this.SmallImgTxt.Text = "1337";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Time:";
+            // 
+            // TimeTxt
+            // 
+            this.TimeTxt.Location = new System.Drawing.Point(100, 184);
+            this.TimeTxt.Name = "TimeTxt";
+            this.TimeTxt.Size = new System.Drawing.Size(165, 20);
+            this.TimeTxt.TabIndex = 1;
+            this.TimeTxt.Text = "September 11, 2169 5:17:37 PM";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Details:";
+            // 
+            // details
+            // 
+            this.details.Location = new System.Drawing.Point(100, 30);
+            this.details.Name = "details";
+            this.details.Size = new System.Drawing.Size(165, 20);
+            this.details.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 211);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Button 1 Text:";
+            // 
+            // Btn1Txt
+            // 
+            this.Btn1Txt.Location = new System.Drawing.Point(99, 211);
+            this.Btn1Txt.Name = "Btn1Txt";
+            this.Btn1Txt.Size = new System.Drawing.Size(165, 20);
+            this.Btn1Txt.TabIndex = 1;
+            this.Btn1Txt.Text = "Sample Text";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(4, 239);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Button 1 URL:";
+            // 
+            // Btn1URL
+            // 
+            this.Btn1URL.Location = new System.Drawing.Point(100, 239);
+            this.Btn1URL.Name = "Btn1URL";
+            this.Btn1URL.Size = new System.Drawing.Size(165, 20);
+            this.Btn1URL.TabIndex = 1;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(3, 265);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(74, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Button 2 Text:";
+            // 
+            // Btn2Txt
+            // 
+            this.Btn2Txt.Location = new System.Drawing.Point(99, 265);
+            this.Btn2Txt.Name = "Btn2Txt";
+            this.Btn2Txt.Size = new System.Drawing.Size(165, 20);
+            this.Btn2Txt.TabIndex = 1;
+            this.Btn2Txt.Text = "Yeet";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(3, 292);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 13);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Button 2 URL:";
+            // 
+            // Btn2URL
+            // 
+            this.Btn2URL.Location = new System.Drawing.Point(99, 292);
+            this.Btn2URL.Name = "Btn2URL";
+            this.Btn2URL.Size = new System.Drawing.Size(165, 20);
+            this.Btn2URL.TabIndex = 1;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(3, 318);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(48, 13);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Party ID:";
+            // 
+            // PartyIDTxt
+            // 
+            this.PartyIDTxt.Location = new System.Drawing.Point(99, 318);
+            this.PartyIDTxt.Name = "PartyIDTxt";
+            this.PartyIDTxt.Size = new System.Drawing.Size(165, 20);
+            this.PartyIDTxt.TabIndex = 1;
+            this.PartyIDTxt.Text = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(3, 344);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(64, 13);
+            this.label14.TabIndex = 0;
+            this.label14.Text = "Min Players:";
+            // 
+            // MinPlayers
+            // 
+            this.MinPlayers.Location = new System.Drawing.Point(99, 344);
+            this.MinPlayers.Name = "MinPlayers";
+            this.MinPlayers.Size = new System.Drawing.Size(165, 20);
+            this.MinPlayers.TabIndex = 1;
+            this.MinPlayers.Text = "1";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(3, 370);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(67, 13);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "Max Players:";
+            // 
+            // MaxPlayers
+            // 
+            this.MaxPlayers.Location = new System.Drawing.Point(99, 370);
+            this.MaxPlayers.Name = "MaxPlayers";
+            this.MaxPlayers.Size = new System.Drawing.Size(165, 20);
+            this.MaxPlayers.TabIndex = 1;
+            this.MaxPlayers.Text = "1337";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.StartBtn);
+            this.Name = "Form1";
+            this.Text = "Discord Rich Presence Hub";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox details;
+        private System.Windows.Forms.TextBox TimeTxt;
+        private System.Windows.Forms.TextBox SmallImg;
+        private System.Windows.Forms.TextBox LargeImg;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SmallImgTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox LargeImgTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox state;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IDTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Btn2URL;
+        private System.Windows.Forms.TextBox Btn2Txt;
+        private System.Windows.Forms.TextBox Btn1URL;
+        private System.Windows.Forms.TextBox Btn1Txt;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox MaxPlayers;
+        private System.Windows.Forms.TextBox MinPlayers;
+        private System.Windows.Forms.TextBox PartyIDTxt;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ RichPresenceHub/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using DiscordRPC;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Button = DiscordRPC.Button;
+
+namespace RichPresenceHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartBtn_Click(object sender, EventArgs e)
+        {
+            // Initialize the Discord rich presence client.
+            var client = new DiscordRpcClient(IDTxt.Text);
+            client.Initialize();
+
+            // Set the initial rich presence information to display on Discord.
+            var presence = new RichPresence
+            {
+                Details = details.Text,
+                State = state.Text,
+                Party = new Party()
+                {
+                    ID = PartyIDTxt.Text,
+                    Size = Convert.ToInt16(MinPlayers.Text),
+                    Max = Convert.ToInt16(MaxPlayers.Text)
+                },
+                Timestamps = Timestamps.Now,
+                Assets = new Assets
+                {
+                    LargeImageKey = LargeImg.Text,
+                    LargeImageText = LargeImgTxt.Text,
+                    SmallImageKey = SmallImg.Text,
+                    SmallImageText = SmallImgTxt.Text,
+
+                },
+                Buttons = new Button[]
+                {
+                    new Button
+                    {
+                        Label = Btn1Txt.Text,
+                        Url = Btn1URL.Text,
+                    },
+                    new Button
+                    {
+                        Label = Btn2Txt.Text,
+                        Url = Btn2URL.Text,
+                    }
+
+                }
+            };
+            client.SetPresence(presence);
+
+            // Start the timer to track elapsed time.
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Update the rich presence information every 10 seconds.
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                presence.Timestamps = new Timestamps
+                {
+                    Start = DateTime.UtcNow - stopwatch.Elapsed,
+                    End = DateTime.Parse(TimeTxt.Text)
+                };
+                client.SetPresence(presence);
+            }
+        }
+    }
+}
